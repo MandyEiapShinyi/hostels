f0899eff85c00c62a9962e68226fedf1f97dd57a
mandy <mandyshinyi@gmail.com>
2024-10-15 15:44:48 +0800

    no message

--- routes/web.php
+++ routes/web.php
@@ -17,27 +17,3 @@ Route::post('/adduser', [UserController::class, 'adduser']);
 //login form
 Route::get('/login', [UserController::class, 'login'])->name('login');
 Route::post('/user', [UserController::class, 'user']);
-
-<<<<<<< HEAD
-//index
-Route::get('/index', [IndexController::class, 'index']);
-
-//review
-Route::post('/review', [IndexController::class, 'review']);
-
-//room
-Route::get('/contect', [IndexController::class, 'contect']);
-
-//logout
-Route::post('/logout', [UserController::class, 'logout'])->name('logout');
-=======
-Route::get('/index', [   IndexController::class, 'index']);
-Route::post('/logout', [UserController::class, 'logout'])->name('logout');
-
-Route::get('/adminlogin', [LoginController::class, 'showLoginForm'])->name('showLoginForm');
-Route::post('/login', [LoginController::class, 'login']);
-// Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
-
-// Admin panel route
-Route::get('/admin/panel', [LoginController::class, 'adminindex']);
->>>>>>> origin/admin-panel
